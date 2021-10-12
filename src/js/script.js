d5b8a338881a75017d65875e863e53412b8e3e4b
ilya9293 <ilya9293@ukr.net>
2021-10-12 20:41:57 +0300

    настроил слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
    $('.slider').slick({
+      arrows: false,
       dots: true,
       infinite: true,
       speed: 300,
       slidesToShow: 1,
+      adaptiveHeight: true,
       centerMode: true,
-      variableWidth: false
+      variableWidth: true
    });
- });
+});
